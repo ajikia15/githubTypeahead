2f58a4d1387594f8870566c4526a05d86047b9c3
ajikia15 <aleksandrej15@gmail.com>
2024-03-04 18:52:35 +0400

    Refactor Searchbar component to include dynamic placeholder

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,25 +1,33 @@
-import { useAutoAnimate } from "@formkit/auto-animate/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-enum InputState {
-  Active = "w-3/5 border",
-  Inactive = "w-2/5",
-}
+const activeInputStyle = "w-3/5 border top-1/4";
+const inactiveInputStyle = "w-2/5 top-1/2 -translate-y-1/2";
+
 export const Searchbar = () => {
   const [isActive, setIsActive] = useState(false);
-  const [parent, enableAnimations] = useAutoAnimate();
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCounter((prevCounter) => (prevCounter + 1) % 4);
+    }, 700);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const placeholder = `Search${".".repeat(counter)}`;
+
+  const classes = `${
+    isActive ? activeInputStyle : inactiveInputStyle
+  } absolute left-1/2 -translate-x-1/2 duration-[350ms] bg-neutral-800 rounded-lg p-5 mx-auto transition-all text-3xl caret-neutral-400 border-neutral-600 outline-none font-bold`;
 
   return (
-    <>
-      <input
-        type="text"
-        placeholder="Search..."
-        className={`${
-          isActive ? InputState.Active : InputState.Inactive
-        } absolute bg-neutral-800 rounded-lg p-54 mx-auto transition-all text-3xl p-5 caret-neutral-400 border-neutral-600 outline-none font-bold`}
-        onFocus={() => setIsActive(true)}
-        onBlur={() => setIsActive(false)}
-      />
-    </>
+    <input
+      type="text"
+      placeholder={isActive ? "" : placeholder}
+      className={classes}
+      onFocus={() => setIsActive(true)}
+      onBlur={() => setIsActive(false)}
+    />
   );
 };
